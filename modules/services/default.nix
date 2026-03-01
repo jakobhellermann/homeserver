8c74c1e15b24d37d5faa524b6a9660e6d65e89f3
Jakob Hellermann <jakob.hellermann@protonmail.com>
2026-03-01 22:56:34 +0100

    services(dns): setup unbound

--- modules/services/default.nix
+++ modules/services/default.nix
@@ -1,8 +1,6 @@
 { lib, ... }:
 
 {
-  options.my.services = lib.mkOption { };
-
   options.my.domains = lib.mkOption {
     type = lib.types.listOf lib.types.str;
     description = "List of base domains for services (e.g., mel.local, mel.home)";
